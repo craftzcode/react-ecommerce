2a84444029d89a1529a7cd4ee6c20b334b1d5f0d
craftzcode <tomymaritano@gmail.com>
2021-12-04 20:29:36 +0100

    chakraUI

---
+++ src/components/Navbar.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+export default function Navbar() {
+	return (
+		<div>
+			<h1>Tech-ecommerce</h1>
+			<ul>
+				<li>HomePage</li>
+				<li>Products</li>
+				<li>Contact</li>
+			</ul>
+		</div>
+	)
+}
