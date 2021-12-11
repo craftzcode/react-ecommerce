12c4cbd63db8041a270598219b138e069b14640a
craftzcode <tomymaritano@gmail.com>
2021-12-11 14:29:25 +0100

    push

--- src/index.js
+++ src/index.js
@@ -5,11 +5,7 @@ import App from './App';
 import { ChakraProvider } from '@chakra-ui/react';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <ChakraProvider>
-      <App />
-    </ChakraProvider>
-  </React.StrictMode>,
+
   document.getElementById('root')
 );
 
